eb8f613e2d3830df2ca98bf95800029eba3d6e19
cattandrej <45920569+cattandrej@users.noreply.github.com>
2020-02-09 16:38:42 +0100

    protocol add animation

--- protocol/js/script.js
+++ protocol/js/script.js
@@ -1,144 +1,83 @@
-$("g#Ivana_Anne").hide();
-$("g#Pipi_Natalie").hide();
-$("g#Arthur_Andrej_h128").hide();
-$("g#Arthur_Andrej_saed").hide();
-$("g#Pretty_Yueling_48_hs").hide();
-$("g#Pretty_Yueling_4_hs").hide();
-$("g#Arthur_Sherlock_200").hide();
-$("g#Arthur_Sherlock_2000").hide();
-$("g#Andrej-jones").hide();
-$("g#Pipi_Shia").hide();
+var popupList = [
+  "g#Ivana_Anne",
+  "g#Pipi_Natalie",
+  "g#Arthur_Andrej_h128",
+  "g#Arthur_Andrej_saed",
+  "g#Pretty_Yueling_48_hs",
+  "g#Pretty_Yueling_4_hs",
+  "g#Arthur_Sherlock_200",
+  "g#Arthur_Sherlock_2000",
+  "g#Andrej-jones",
+  "g#Pipi_Shia"
+]
+
+$(document).ready(function(){
+  resizeSidebar();
+  hideAll(popupList);
+});
+
+window.addEventListener("resize", resizeSidebar);
 
 $("#Capa_1").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
+  hideAll(popupList);
 });
 
 $("g#_x32_").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").toggle();
+  toggleElement("g#Pipi_Shia", popupList);
 });
+
 $("g#_x33_").click(function() {
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Ivana_Anne").toggle();
+  toggleElement("g#Ivana_Anne", popupList);
 });
+
 $("g#_x34_").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Arthur_Sherlock_200").toggle();
+  toggleElement("g#Arthur_Sherlock_200", popupList);
 });
+
 $("g#_x35_").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Arthur_Sherlock_2000").toggle();
+  toggleElement("g#Arthur_Sherlock_2000", popupList);
 });
+
 $("g#_x35_-1").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Pipi_Natalie").toggle();
+  toggleElement("g#Pipi_Natalie", popupList);
 });
+
 $("g#_x36_").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Andrej-jones").toggle();
+  toggleElement("g#Andrej-jones", popupList);
 });
+
 $("g#_x38_").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Pretty_Yueling_4_hs").toggle();
+  toggleElement("g#Pretty_Yueling_4_hs", popupList);
 });
+
 $("g#_x38_-2").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Pretty_Yueling_48_hs").toggle();
+  toggleElement("g#Pretty_Yueling_48_hs", popupList);
 });
+
 $("g#_x39_").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_h128").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Arthur_Andrej_saed").toggle();
+  toggleElement("g#Arthur_Andrej_saed", popupList);
 });
+
 $("g#_x39_-2").click(function() {
-  $("g#Ivana_Anne").hide();
-  $("g#Pipi_Natalie").hide();
-  $("g#Arthur_Andrej_saed").hide();
-  $("g#Pretty_Yueling_48_hs").hide();
-  $("g#Pretty_Yueling_4_hs").hide();
-  $("g#Arthur_Sherlock_200").hide();
-  $("g#Arthur_Sherlock_2000").hide();
-  $("g#Andrej-jones").hide();
-  $("g#Pipi_Shia").hide();
-  $("g#Arthur_Andrej_h128").toggle();
+  toggleElement("g#Arthur_Andrej_h128", popupList);
 });
+
+function resizeSidebar() {
+  $(".protocol-sidebadr").css("height", $(".container-svg").css("height"));
+}
+
+function hideAll(elementList) {
+  for (var i = 0; i < elementList.length; i++) {
+    $(elementList[i]).hide();
+  }
+}
+
+function toggleElement(element, elementList) {
+  for (var i = 0; i < elementList.length; i++) {
+    if (elementList[i] === element) {
+      $(elementList[i]).toggle();
+    } else {
+      $(elementList[i]).hide();
+    }
+  }
+}
